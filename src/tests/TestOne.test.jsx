11b18246c48ebb2e7c84d0b030b7dbee362b233d
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-06-08 14:41:24 +0200

    Set up basic file with some testing involved

---
+++ src/tests/TestOne.test.jsx
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "../App.jsx";
+
+describe("testing whether the thing works", () => {
+  it("true to be true", () => {
+    expect(true).toBe(true);
+  });
+
+  it("expect false to be false", () => {
+    expect(false).toBe(false);
+  });
+});
+
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch("Vite + React");
+  });
+});
