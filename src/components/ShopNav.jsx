31ee2855a21f7dc3d7cc95d211e214d15f113698
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-06-10 15:00:26 +0200

    Added shop navigation bar

---
+++ src/components/ShopNav.jsx
@@ -0,0 +1,14 @@
+// navigation bar to allow users to switch categories while in the shop
+
+function ShopNav() {
+  return (
+    <div className="shopNav">
+      <button>Men's clothing</button>
+      <button>Women's clothing</button>
+      <button>Jewelery</button>
+      <button>Electronics</button>
+    </div>
+  );
+}
+
+export { ShopNav };
